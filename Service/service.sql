8cf7ea15ced79e66e1d1b44fa6c1d3798f61d093
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2016-03-21 15:27:00 +0100

    updated sql schema

---
+++ Service/service.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 21. Mrz 2016 um 15:21
+-- Server Version: 5.6.21
+-- PHP-Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `wikipedia_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `artikel`
+--
+
+DROP TABLE IF EXISTS `artikel`;
+CREATE TABLE IF NOT EXISTS `artikel` (
+  `id` char(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `titel` varchar(250) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `text` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `tags` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `datum` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `nutzer`
+--
+
+DROP TABLE IF EXISTS `nutzer`;
+CREATE TABLE IF NOT EXISTS `nutzer` (
+  `id` int(11) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `nutzer_interessen`
+--
+
+DROP TABLE IF EXISTS `nutzer_interessen`;
+CREATE TABLE IF NOT EXISTS `nutzer_interessen` (
+  `id` int(11) NOT NULL,
+  `interesse` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `personalisierung`
+--
+
+DROP TABLE IF EXISTS `personalisierung`;
+CREATE TABLE IF NOT EXISTS `personalisierung` (
+  `articleid` char(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `userid` int(11) NOT NULL,
+  `score` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `vector_alle`
+--
+
+DROP TABLE IF EXISTS `vector_alle`;
+CREATE TABLE IF NOT EXISTS `vector_alle` (
+  `id` char(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `wikipediaid` int(11) NOT NULL,
+  `score` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `vector_ohne_personen`
+--
+
+DROP TABLE IF EXISTS `vector_ohne_personen`;
+CREATE TABLE IF NOT EXISTS `vector_ohne_personen` (
+  `id` char(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `wikipediaid` int(11) NOT NULL,
+  `score` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `vector_personen`
+--
+
+DROP TABLE IF EXISTS `vector_personen`;
+CREATE TABLE IF NOT EXISTS `vector_personen` (
+  `id` char(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `wikipediaid` int(11) NOT NULL,
+  `score` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `wikipedia`
+--
+
+DROP TABLE IF EXISTS `wikipedia`;
+CREATE TABLE IF NOT EXISTS `wikipedia` (
+  `id` int(10) unsigned NOT NULL,
+  `person` int(10) unsigned NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `body` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `artikel`
+--
+ALTER TABLE `artikel`
+ ADD PRIMARY KEY (`id`), ADD KEY `datum` (`datum`);
+
+--
+-- Indizes für die Tabelle `nutzer`
+--
+ALTER TABLE `nutzer`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `nutzer_interessen`
+--
+ALTER TABLE `nutzer_interessen`
+ ADD KEY `id` (`id`), ADD KEY `id_2` (`id`);
+
+--
+-- Indizes für die Tabelle `personalisierung`
+--
+ALTER TABLE `personalisierung`
+ ADD KEY `articleid` (`articleid`), ADD KEY `userid` (`userid`);
+
+--
+-- Indizes für die Tabelle `vector_alle`
+--
+ALTER TABLE `vector_alle`
+ ADD KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `vector_ohne_personen`
+--
+ALTER TABLE `vector_ohne_personen`
+ ADD KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `vector_personen`
+--
+ALTER TABLE `vector_personen`
+ ADD KEY `id` (`id`);
+
+--
+-- Indizes für die Tabelle `wikipedia`
+--
+ALTER TABLE `wikipedia`
+ ADD PRIMARY KEY (`id`), ADD FULLTEXT KEY `body` (`body`);
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `nutzer_interessen`
+--
+ALTER TABLE `nutzer_interessen`
+ADD CONSTRAINT `nutzer_interessen_ibfk_1` FOREIGN KEY (`id`) REFERENCES `nutzer` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `personalisierung`
+--
+ALTER TABLE `personalisierung`
+ADD CONSTRAINT `personalisierung_ibfk_1` FOREIGN KEY (`articleid`) REFERENCES `artikel` (`id`) ON DELETE CASCADE,
+ADD CONSTRAINT `personalisierung_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `nutzer` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `vector_alle`
+--
+ALTER TABLE `vector_alle`
+ADD CONSTRAINT `vector_alle_ibfk_1` FOREIGN KEY (`id`) REFERENCES `artikel` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `vector_ohne_personen`
+--
+ALTER TABLE `vector_ohne_personen`
+ADD CONSTRAINT `vector_ohne_personen_ibfk_1` FOREIGN KEY (`id`) REFERENCES `artikel` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `vector_personen`
+--
+ALTER TABLE `vector_personen`
+ADD CONSTRAINT `vector_personen_ibfk_1` FOREIGN KEY (`id`) REFERENCES `artikel` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
