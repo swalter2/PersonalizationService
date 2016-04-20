bea4dee7be4a13a1ceb4746b295762a1ae634886
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2016-04-20 10:50:22 +0200

    updated

--- Service/xmlimporter.py
+++ Service/xmlimporter.py
@@ -115,7 +115,7 @@ class XMLImporter:
 
         article_vector = {}
         for input in hm_tags:
-            tmp_vector = XMLImporter.database.getarticlesfromwikipedia(mode, input, 25)
+            tmp_vector = XMLImporter.database.getarticlesfromwikipedia(mode, input, 200)
             for title in tmp_vector:
                 if title in article_vector:
                     # in the moment only addition of the scores, maybe also try averaging of the scores
