e27b65ef15ea353b92e3fe511b020af724a43663
Fabian Kaupmann <Fabian@Fabians-MacBook-Pro-2.local>
2016-04-30 13:47:19 +0200

    annotation_comparison mit esa added

--- Service/feature_extraction.py
+++ Service/feature_extraction.py
@@ -22,6 +22,28 @@ def normalize_article_ressort_to_dict(article_ressort,ressort_list):
             result[ressort] = 0
     return result
 
+#normalisiert seiten auf die von philipp recherchierten bereiche
+def normalize_pages(self):
+    dict = {'1':0,'2':0,'3':0,'4-5':0,'6-7':0,'8':0,'9-16':0,'17-24':0,'25+':0}
+    if self.page == 1:
+        dict['1'] = 1
+    elif self.page == 2:
+        dict['2'] = 1
+    elif self.page == 3:
+        dict['3'] = 1
+    elif self.page < 6:
+        dict['4-5'] = 1
+    elif self.page < 8:
+        dict['6-7'] = 1
+    elif self.page == 8:
+        dict['8'] = 1
+    elif self.page < 17:
+        dict['9-16'] = 1
+    elif self.page < 25:
+        dict['17-24'] = 1
+    else:
+        dict['25+'] = 1
+    return dict
 
 
 
@@ -108,33 +130,85 @@ def compute_cross_features(user_age,user_sex,user_education,article_ressort,arti
 
     return cf_age_ressort,cf_sex_ressort,cf_edu_ressort,cf_age_page,cf_sex_page,cf_edu_page
 
+def compute_annotation_esa_comparison(article, user_annotations, mode='pos'):
 
-def compute_general_feature_dict(user_age,user_sex,user_education,article_ressort,article_normalized_page,
-                           ressort_list,pages_list,age_list,sexes_list,edu_list):
+    cos_sims = []
 
-    ressort_prior_dict = normalize_article_ressort_to_dict(article_ressort,ressort_list)
+    user_annotations = []
 
-    cf_age_ressort, cf_sex_ressort, cf_edu_ressort, cf_age_page, cf_sex_page, cf_edu_page = compute_cross_features(
-        user_age,user_sex,user_education,article_ressort,article_normalized_page,ressort_list,pages_list,age_list,sexes_list,edu_list
-    )
-    #weitere feature-extraction hier einfügen
+    for article,annotation in user_annotations:
+        if mode == 'pos':
+            if annotation == 4:
+                #Todo: esa_vectors für die zwei Artikel aus DB
+                esa_vec_current_article = []
+                esa_vec_article = []
+                cos_sim = general.calcualtecos(esa_vec_current_article,esa_vec_article)
+                cos_sims.append(cos_sim)
 
-    #features in einem Dict zusammenfassen
-    feature_dict = {}
-
-    feature_dict.update(ressort_prior_dict)
-    feature_dict.update(cf_age_ressort)
-    feature_dict.update(cf_sex_ressort)
-    feature_dict.update(cf_edu_ressort)
-    feature_dict.update(cf_age_page)
-    feature_dict.update(cf_sex_page)
-    feature_dict.update(cf_edu_page)
-
-    return feature_dict
-
+        #nur negativ bewertete artikel nutzen
+        elif mode == 'neg':
+            if annotation == 1:
+                #Todo: esa_vectors für die zwei Artikel aus DB
+                esa_vec_current_article = []
+                esa_vec_article = []
+                cos_sim = general.calcualtecos(esa_vec_current_article,esa_vec_article)
+                cos_sims.append(cos_sim)
 
 
-####    AB HIER ESA FEATURES, voerst nicht relevant     ########
+    return np.min(cos_sims),np.max(cos_sims),np.mean(cos_sims)
+
+def compute_general_feature_dict(user_list, ressort_list,pages_list,age_list,sexes_list,edu_list):
+
+    feature_vector = []
+
+    for user in user_list:
+        #Todo: aus Datenbank holen
+        user_age = False
+        user_sex = False
+        user_education = False
+
+        user_annotations = []
+
+        for article, annotation in user_annotations:
+            #Todo: aus Datenbank holen
+            article_ressort = False
+            article_normalized_page = normalize_pages(False)
+
+
+            ressort_prior_dict = normalize_article_ressort_to_dict(article_ressort,ressort_list)
+
+            cf_age_ressort, cf_sex_ressort, cf_edu_ressort, cf_age_page, cf_sex_page, cf_edu_page = compute_cross_features(
+                user_age,user_sex,user_education,article_ressort,article_normalized_page,ressort_list,pages_list,age_list,sexes_list,edu_list
+            )
+
+            annotation_comparison_min_pos, annotation_comparison_max_pos, annotation_comparison_mean_pos = compute_annotation_esa_comparison(article,user_annotations)
+            annotation_comparison_min_neg, annotation_comparison_max_neg, annotation_comparison_mean_neg = compute_annotation_esa_comparison(article,user_annotations)
+
+            annotation_comparison_dict = {'comparison_min_pos':annotation_comparison_min_pos, 'comparison_max_pos':annotation_comparison_max_pos,
+                                          'comparison_mean_pos':annotation_comparison_mean_pos,'comparison_min_neg':annotation_comparison_min_neg,
+                                          'comparison_max_neg':annotation_comparison_max_neg, 'comparison_mean_neg':annotation_comparison_mean_neg}
+
+            #weitere feature-extraction hier einfügen
+
+            #features in einem Dict zusammenfassen
+            feature_dict = {}
+
+            feature_dict.update(ressort_prior_dict)
+            feature_dict.update(cf_age_ressort)
+            feature_dict.update(cf_sex_ressort)
+            feature_dict.update(cf_edu_ressort)
+            feature_dict.update(cf_age_page)
+            feature_dict.update(cf_sex_page)
+            feature_dict.update(cf_edu_page)
+            feature_dict.update(annotation_comparison_dict)
+
+            feature_vector.append(feature_dict)
+
+    return feature_vector
+
+
+
+####    AB HIER ESA FEATURES, voerst nicht relevant, da vom Service selbst schon implementiert     ########
 
 #berechne cosinus-vergleich von artikel und user. berechne zusätzlich, ob score besser
 # als der durchschnitt der aktuellen Ausgabe
