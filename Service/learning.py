180afbbe9f209dbdb50b7f0ec30b3e887a6ee854
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2017-01-13 10:22:08 +0100

    fixed bug in learning

--- Service/learning.py
+++ Service/learning.py
@@ -190,8 +190,7 @@ class Learning:
         results = Learning.learn({}, articleids, userid, ALL_ARTICLES, user_informations[userid],article_informations)
         for articleid in results:
             score = results[articleid]
-            if score > 0.0001:
-                Learning.database.add_personalization_all_userarticle(userid, articleid, score)
+            Learning.database.add_personalization_all_userarticle(userid, articleid, score)
 
 
 
